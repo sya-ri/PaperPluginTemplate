6d7a75f83942217d12bb1d86173a3806dd5d0dcf
sya-ri <sya79lua@gmail.com>
2021-02-23 17:01:19 +0900

    EasySpigotAPI を v1.3.4 に更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ configurations["implementation"].extendsFrom(shadowImplementation)
 dependencies {
     shadowImplementation(kotlin("stdlib"))
     implementation("com.destroystokyo.paper:paper-api:1.16.5-R0.1-SNAPSHOT")
-    shadowImplementation("com.github.sya-ri:EasySpigotAPI:1.3.0") {
+    shadowImplementation("com.github.sya-ri:EasySpigotAPI:1.3.4") {
         exclude(group = "org.spigotmc", module = "spigot-api")
     }
 }
